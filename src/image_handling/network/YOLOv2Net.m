7f280d3f0d443df51db2202d9bc6d714199bf339
dennisrotondi <dennisrotondi6@gmail.com>
2022-04-28 22:09:20 +0200

    network architecture fix

--- src/image_handling/network/YOLOv2Net.m
+++ src/image_handling/network/YOLOv2Net.m
@@ -26,40 +26,6 @@ classdef YOLOv2Net
              
          end 
         
-         function  [lgraph , obj] = createLayers(obj)
-            addpath('src/yolo/utilities'); 
-            
-            disp("Creating layer..."); 
-            layer = CreateLayers(); 
-            
-            lgraph = layerGraph(layer); 
-            disp("Layer Graph Created!"); 
-            
-            Anchors = [41, 116; 67, 37; 49, 49; 53, 53]; 
-            lgraph = yolov2Layers([270, 270, 3], 2, Anchors, lgraph, 'relu_4');  
-            obj.LGraph = lgraph;
-            
-         end
-       
-        %% Train the neural network from the data store 
-        function obj = train(obj)
-           
-            
-            lgraph = obj.LGraph; 
-
-            options = trainingOptions('adam',...
-                "InitialLearnRate", 0.0001 ,...
-                "Verbose", true,...
-                "MiniBatchSize", 30, "MaxEpochs",100, ...
-                'Shuffle',"every-epoch", "VerboseFrequency",30);            
-            
-            ds = obj.DataStore; 
-            [detectorYolo2, info] = trainYOLOv2ObjectDetector(ds, lgraph, options); 
-            
-            obj.Detector = detectorYolo2; 
-           
-        end
-        
         function [img, bboxes, scores, labels] = predict(obj, image)
            img = imresize(image, [270,270]); % Resize (Encode) 
            [bboxes, scores, labels] = detect(obj.Detector, img); 
