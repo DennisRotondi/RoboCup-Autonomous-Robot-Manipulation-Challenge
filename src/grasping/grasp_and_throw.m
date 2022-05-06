4757cc02ea72f6f737f5510e9bb32a8834009164
fabioscap <fabiovt_99@hotmail.it>
2022-05-06 17:06:23 +0200

    grasp

--- src/grasping/grasp_and_throw.m
+++ src/grasping/grasp_and_throw.m
@@ -1,6 +1,11 @@
-function outputArg1 = grasp_and_throw(dir,point)
-%GOTO_PLACE Summary of this function goes here
-%   NEED IMPLEMENTATION
-outputArg1 = 0
+function desiredPose = grasp_and_throw(dir,point,label)
+    % end effector orientation
+    Ree = grasp_orientation(dir,0); % TODO set alpha
+    % end effector position
+    tee = point;
+
+    desiredPose = [tee; rotm2eul(Ree,"xyz")'];
+
+    % goto ...
 end
 
