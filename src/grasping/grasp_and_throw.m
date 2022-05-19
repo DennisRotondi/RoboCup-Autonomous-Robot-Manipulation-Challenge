2bf891acbe52be6e1c2c4c6d3857274da9855919
fabioscap <fabiovt_99@hotmail.it>
2022-05-19 10:22:29 +0200

    grasp and throw

--- src/grasping/grasp_and_throw.m
+++ src/grasping/grasp_and_throw.m
@@ -1,11 +1,26 @@
-function desiredPose = grasp_and_throw(dir,point,label)
+function desired_pose = grasp_and_throw(dir,point,label,robot)
+    global trajAct trajGoal jointSub
     % end effector orientation
     Ree = grasp_orientation(dir,0); % TODO set alpha
     % end effector position
     tee = point;
 
-    desiredPose = [tee; rotm2eul(Ree,"xyz")'];
+    desired_pose = [tee; rotm2eul(Ree,"xyz")'];
+    
+    goto = @(pose) (moveGripperTo(robot,trajAct,trajGoal,jointSub,pose(1:3)',pose(4:6)'));
 
-    % goto ...
+    approach_pose = desired_pose + [Ree*[0;0;-0.2]; [0;0;0]];
+    
+    goto(approach_pose);
+    goto(desired_pose);
+    SLActivateGripper(1);
+    goto(approach_pose);
+    if label == "can"
+        goto(can_bin);
+    else
+        goto(bottle_bin)
+    end
+    SLActivateGripper(0);
+
 end
 
