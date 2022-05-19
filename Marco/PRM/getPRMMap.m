900defe4f591eb8367d1aeebf9899817023060b1
dennisrotondi <dennisrotondi6@gmail.com>
2022-05-19 12:11:44 +0200

    small fix

--- Marco/PRM/getPRMMap.m
+++
@@ -1,7 +0,0 @@
-clc;clear;close all;
-load('exampleHelperKINOVAGen3GripperROSGazebo.mat');
-
-mapKinova = getPRMStar(robot,5000);
-plot(mapKinova);
-
-save('mapKinova.mat','mapKinova');
--- Marco/approxDet/InvKinPinv_optim.m
+++
@@ -1,9 +0,0 @@
-function q_d = InvKinPinv_optim(r_d,J, gradH)
-%INV_KIN_PINV_OPTIM
-    size_J = size(J);
-    n_q = size_J(2);
-    q_d = pinv(J)*r_d+(eye(n_q)-pinv(J)*J)*gradH*10;
-end
-
-
-
